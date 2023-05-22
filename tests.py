d3a45251c68275202f3d16f4bb2af313b30257df
ZhannaShum <shumilovajeanna@gmail.com>
2023-05-22 19:18:51 +0300

    Тесты к первому допу

--- tests.py
+++ tests.py
@@ -1,45 +1,85 @@
 import unittest
 import os
+import tempfile
+import shutil
 from file_utils import get_size, check_file_type, get_space_details
-from draw_utils import prepare_data_for_chart, make_autopct
+from draw_utils import make_autopct
 
 
-class TestUtils(unittest.TestCase):
+class TestFileUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
 
     def test_get_size(self):
-        size = get_size(os.path.dirname(os.path.abspath(__file__)))
-        self.assertTrue(isinstance(size, int))
+        # Создаем тестовый файл размером 1024 байта
+        test_file = os.path.join(self.test_dir, 'test_file')
+        with open(test_file, 'wb') as f:
+            f.write(os.urandom(1024))
+
+        # Проверяем, что функция get_size возвращает правильный размер
+        self.assertEqual(get_size(self.test_dir), 1024)
 
     def test_check_file_type(self):
-        self.assertEqual(check_file_type("test.jpg"), 'Images')
-        self.assertEqual(check_file_type("test.mp4"), 'Videos')
-        self.assertEqual(check_file_type("test.mp3"), 'Audio')
-        self.assertEqual(check_file_type("test.doc"), 'Documents')
-        self.assertEqual(check_file_type("test.xyz"), 'Others')
+        # Создаем тестовые файлы различных типов
+        test_files = ['test.doc', 'test.mp3', 'test.png', 'test.py',
+                      'unknown.xyz']
+        for file_name in test_files:
+            with open(os.path.join(self.test_dir, file_name), 'wb') as f:
+                f.write(b'')
 
-    def test_get_space_details(self):
-        details = get_space_details(os.path.dirname(os.path.abspath(__file__)))
-        self.assertTrue(isinstance(details, dict))
-        self.assertTrue('Images' in details and 'Videos' in details and
-                        'Audio' in details and 'Documents' in details and
-                        'Others' in details)
+        # Проверяем, что функция check_file_type возвращает правильные типы файлов
+        self.assertEqual(check_file_type('test.doc'), ('Documents', '*.doc'))
+        self.assertEqual(check_file_type('test.mp3'), ('Audio', '*.mp3'))
+        self.assertEqual(check_file_type('test.png'), ('Images', '*.png'))
+        self.assertEqual(check_file_type('test.py'), ('Programs', '*.py'))
 
     def test_prepare_data_for_chart(self):
-        file_types = {'Images': 1024, 'Videos': 2048, 'Audio': 3072,
-                      'Documents': 4096, 'Others': 5120}
-        total = sum(file_types.values())
-        free = 1024
-        sizes, labels = prepare_data_for_chart(file_types, total, free)
-        self.assertEqual(len(sizes),
-                         len(labels))
-        self.assertEqual(labels[-1],
-                         'Free space')
+        # Создаем тестовые данные для проверки функции
+        file_types = {'Images': 1000, 'Videos': 2000, 'Audio': 1500}
+        total = 10000
+        free = 5500
+
 
     def test_make_autopct(self):
-        values = [1, 2, 3, 4, 5]
-        autopct = make_autopct(values)
-        self.assertEqual(autopct(20),
-                         '20.00%  (3.00 GB)')
+        # Создаем тестовые данные для проверки функции
+        values = [50, 25, 25]
+
+        # Проверяем, что функция make_autopct возвращает правильные значения
+        autopct_func = make_autopct(values)
+        self.assertEqual(autopct_func(20), '20.00 GB')
+        self.assertEqual(autopct_func(10), '10.00 GB')
+        self.assertEqual(autopct_func(5), '5.00 GB')
+
+    def test_get_space_details(self):
+        # Создаем тестовые файлы различных типов
+        test_files = ['test.doc', 'test.mp3', 'test.png', 'test.py',
+                      'unknown.xyz']
+        for file_name in test_files:
+            with open(os.path.join(self.test_dir, file_name), 'wb') as f:
+                f.write(b'')
+
+        file_types, file_counts = get_space_details(self.test_dir)
+
+        # Проверяем, что функция возвращает правильные размеры и количество файлов
+        self.assertEqual(file_types['Documents'], os.path.getsize(
+            os.path.join(self.test_dir, 'test.doc')))
+        self.assertEqual(file_counts['Documents']['*.doc'], 1)
+        self.assertEqual(file_counts['Others']['.xyz'], 1)
+
+    def test_non_existing_path_get_size(self):
+        # Проверяем, что функция обрабатывает несуществующий путь
+        self.assertEqual(get_size('/non/existing/path'), 0)
+
+    def test_non_existing_path_get_space_details(self):
+        # Проверяем, что функция обрабатывает несуществующий путь
+        file_types, file_counts = get_space_details('/non/existing/path')
+        self.assertEqual(sum(file_types.values()), 0)
+        for counts in file_counts.values():
+            self.assertEqual(sum(counts.values()), 0)
 
 
 if __name__ == '__main__':
